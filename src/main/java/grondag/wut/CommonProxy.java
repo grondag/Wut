0084ec62bdf4c748521cd94afed6c922888d8566
MeatHat <>
2018-09-24 19:32:38 -0700

    created blerb

--- src/main/java/grondag/wut/CommonProxy.java
+++ src/main/java/grondag/wut/CommonProxy.java
@@ -1,14 +1,29 @@
 package grondag.wut;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class CommonProxy 
 {
 	public void preInit(FMLPreInitializationEvent event) 
 	{
 		Wut.setLog(event.getModLog());
+		
+		EntityRegistry.registerModEntity(
+				new ResourceLocation("wut:blerb"), 
+				EntityBlerb.class, 
+				"wut:blerb", 
+				400, 
+				Wut.INSTANCE, 
+				64,
+				10,
+				true, 
+				0xFFFFFF, 
+				0x926AF4);
+		
 	}
 
 	public void init(FMLInitializationEvent event) 
---
+++ src/main/java/grondag/wut/EntityBlerb.java
@@ -0,0 +1,14 @@
+package grondag.wut;
+
+import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.world.World;
+
+public class EntityBlerb extends EntityOcelot
+{
+
+	public EntityBlerb(World worldIn) 
+	{
+		super(worldIn);
+	}
+
+}
